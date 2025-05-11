81a1e8fe06a481f72946069a128eed54cc49f40e
FrankGrillo1991 <fsg12491@aol.com>
2025-05-11 17:03:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -12,4 +12,32 @@ let selectedWord = words[Math.floor(Math.random() * words.length)];
 let correctLetters = [];
 let wrongLetters = [];
 
+function drawHangman(errors) {
+    ctx.clearRect(0, 0, canvas.clientWidth, canvas.height);
+    // base
+    if (errors > 0) ctx.fillRect(10, 240, 180, 10);
+    // pole
+    if (errors > 1) ctx.fillRect(50, 20, 10, 220);
+    // beam
+    if (errors > 2) ctx.fillRect(50, 20, 80, 10);
+    // rope
+    if (errors > 3) ctx.fillRect(130, 20, 2, 20);
+    // head
+    if (errors > 4) {
+        ctx.beginPath();
+        ctx.arc(131, 60, 15, 0, Math.PI * 2);
+        ctx.stroke();
+    }
+    // body
+    if (errors > 5) ctx.fillRect(130, 75, 2, 50);
+    // left arm
+    if (errors > 6) ctx.fillRect(120, 85, 10, 2);
+    // right arm
+    if (errors > 7) ctx.fillRect(132, 85, 10, 2);
+    // left leg
+    if (errors > 8) ctx.fillRect(120, 125, 10, 2);
+    // right leg
+    if (errors > 9) ctx.fillRect(132, 125, 10, 2);
+}
+
 
