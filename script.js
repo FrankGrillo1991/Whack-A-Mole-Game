101d48bee8989745bf55c12463dfc56bc7231cff
FrankGrillo1991 <fsg12491@aol.com>
2025-05-11 16:51:53 -0400

    Update script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const wordEl = document.getElementById('word');
+const wrongLetterEl = document.getElementById('wrong-letters');
+const notification = document.getElementById('notification');
+const popup = document.getElementById('popup');
+const messageEl = document.getElementById('message');
+const playButton = document.getElementById('play-button');
+const canvas = document.getElementById('hangman');
+const ctx = canvas.getContext('2d');
+
+
